91e8e40c963999192c3e8308a822d30d79a22166
Rafael <rfsistemas2@gmail.com>
2026-08-17 22:09:39 -0300

    Ajuste retiradas

--- CasaDoTerno.Web/src/pages/Retiradas.tsx
+++ CasaDoTerno.Web/src/pages/Retiradas.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import api from "../Services/API";
 import { useNavigate } from "react-router-dom";
 import { FileText, Pencil, Printer } from "lucide-react";
